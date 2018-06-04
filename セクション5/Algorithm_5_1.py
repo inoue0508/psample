760008b54fe5a164af93c95f3cce36663b79079e
inoue <inoue@sx0000030540-00.g01.fujitsu.local>
2018-06-04 19:17:12 +0900

    moziretsusousa

---
+++ セクション5/Algorithm_5_1.py
@@ -0,0 +1,41 @@
+'''
+ファイル操作演習の解答
+'''
+class Answer_5_1():
+
+    #定数
+    txtFileName = '/home/inoue/python/python.txt'
+    csvFileName = '/home/inoue/python/python.csv'
+
+    ##-------------------------Step1-----------------------------##
+    def Step1(self):
+        #ファイル読み込み
+        with open(self.txtFileName,'rt') as contents:
+            print(contents.read())
+    
+
+    ##-------------------------Step2-----------------------------##
+    def Step2(self):
+        outputFile = '/home/inoue/python/newPython.txt'
+        with open(outputFile,'wt') as newFile:
+            with open(self.txtFileName,'rt') as contents:
+                newFile.write(contents.readline())
+    
+    ##-------------------------Step3-----------------------------##
+    def Step3(self):
+        import csv
+        with open(self.csvFileName,'r') as contents:
+            for row in contents:
+                print(row)
+
+
+    ##-------------------------Step4-----------------------------##
+    def Step4(self):
+        import pandas as pd
+
+        contents = pd.read_csv(filepath_or_buffer=self.csvFileName)
+        print(contents)
+
+
+        
+
---
+++ セクション5/Call.py
@@ -0,0 +1,12 @@
+'''
+結果確認用のファイルです。
+'''
+
+#各ファイル読み込み
+import Algorithm_5_1 as al_51
+
+
+
+ins = al_51.Answer_5_1()
+ins.Step1()
+
